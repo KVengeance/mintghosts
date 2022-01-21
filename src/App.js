8b5db2d88cd51a5402bb9a48bddc8309145b80cd
Kara Vengeance <karavengeance2@gmail.com>
2022-01-21 17:22:18 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -255,7 +255,7 @@ function App() {
                   margin: "5px",
                 }}
               >
-                Roadmap
+                Info
               </StyledButton>
               <StyledButton
                 style={{
